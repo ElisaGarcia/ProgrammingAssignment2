f79005ea43b5da6c71df1ddb81543fb527b09919
Elisa <e.g.corisco@gmail.com>
2015-03-21 20:31:16 +0100

    developed makeCacheMatrix and cacheSolve
    with these two functions a matrix can be created whose inverse can be
    stored in cache in order that the system does not have to calculate the
    inverse twice for the same data

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function creates a matrix from which the inverse is saved
+## the cache in order that it has not to be calculated twice. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- matrix()
+  set <- function(y) {
+    x <<- y
+    inv <<- matrix()
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse) 
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverse of a matrix created with the function above
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached matrix")
+    return(inverse)
+  }
+  matrix <- x$get()
+  inverse <- solve(x)
+  x$setInverse(inverse)
+  inverse
 }
